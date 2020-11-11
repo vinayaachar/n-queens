7d89b20af8d16774ff9f32a5b74ea557c9a90ee6
jrudio <justinjrudio@gmail.com>
2020-11-10 19:59:08 -0800

    Complete board through diagonal conflicts

--- src/Board.js
+++ src/Board.js
@@ -157,12 +157,50 @@
     //
     // test if a specific major diagonal on this board contains a conflict
     hasMajorDiagonalConflictAt: function(majorDiagonalColumnIndexAtFirstRow) {
-      return false; // fixme
+      var matrix = this.rows();
+
+      for (var i = 0; i < matrix.length; i++) {
+        var col = matrix[i][majorDiagonalColumnIndexAtFirstRow];
+        if (col === 1) {
+
+          return true;
+        }
+      }
+
+      return false;
     },
 
     // test if any major diagonals on this board contain conflicts
     hasAnyMajorDiagonalConflicts: function() {
-      return false; // fixme
+      var tracker = [];
+      var matrix = this.rows();
+
+      for (var i = 0; i < matrix.length; i++) {
+        if (this.hasMajorDiagonalConflictAt(i)) {
+          for (var j = 0; j < matrix.length; j++) {
+            if (matrix[j][i] === 1) {
+              tracker.push(j);
+              break;
+            }
+          }
+        }
+      }
+
+      if (tracker.length < 2) {
+        return false;
+      }
+
+      var temp = tracker[0];
+
+      for (var i = 1; i < tracker.length; i++) {
+        if (tracker[i] > temp) {
+          return true;
+        }
+
+        temp = tracker[i];
+      }
+
+      return false;
     },
 
 
@@ -172,12 +210,51 @@
     //
     // test if a specific minor diagonal on this board contains a conflict
     hasMinorDiagonalConflictAt: function(minorDiagonalColumnIndexAtFirstRow) {
-      return false; // fixme
+      var matrix = this.rows();
+
+      for (var i = 0; i < matrix.length; i++) {
+        var col = matrix[i][minorDiagonalColumnIndexAtFirstRow];
+        if (col === 1) {
+
+          return true;
+        }
+      }
+
+      return false;
     },
 
     // test if any minor diagonals on this board contain conflicts
     hasAnyMinorDiagonalConflicts: function() {
-      return false; // fixme
+      var tracker = [];
+      var matrix = this.rows();
+
+      for (var i = 0; i < matrix.length; i++) {
+        if (this.hasMinorDiagonalConflictAt(i)) {
+          for (var j = 0; j < matrix.length; j++) {
+            if (matrix[j][i] === 1) {
+              tracker.push(j);
+              break;
+            }
+          }
+        }
+      }
+
+      if (tracker.length < 2) {
+        return false;
+      }
+
+      var temp = tracker[0];
+
+      for (var i = 1; i < tracker.length; i++) {
+        if (tracker[i] < temp) {
+          return true;
+        }
+
+        temp = tracker[i];
+      }
+
+      return false;
+
     }
 
     /*--------------------  End of Helper Functions  ---------------------*/
